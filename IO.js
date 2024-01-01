e0f998bac90acbeb54e204d5a13bb6e1c763d064
vanilla028 <alison28@daum.net>
2024-01-01 23:15:08 +0900

    js:

---
+++ IO.js
@@ -0,0 +1,14 @@
+// Javascript 기본 문법
+
+let num; // 변수 선언
+num = 28; // 변수에 데이터 저장
+let number = 34; // 변수 선언과 동시에 데이터 저장
+
+// 변수에 저장된 데이터 불러오기
+console.log(num); // 28
+console.log(number); // 34
+
+// 사칙연산
+let sum = 10 + 1
+let second = sum + 1
+let third = sum + second // 23
