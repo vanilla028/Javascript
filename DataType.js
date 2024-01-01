5b0d8ef3a40baafc1833ee8fb0846071d0dbb723
vanilla028 <alison28@daum.net>
2024-01-01 23:16:23 +0900

    js

---
+++ DataType.js
@@ -0,0 +1,24 @@
+// 데이터 유형
+let id = "KIM" + "RACHEL";
+console.log(id); // KIMRACHEL
+
+// 데이터 타입에 따라 연산이 달라지므로,
+// 변수에 저장된 데이터 타입을 알고 있어야 한다.
+let num = "21";
+let total = num +10;
+console.log(total); // 2110
+
+// Boolean 형
+let first = true;
+let second = false;
+
+// 배열
+let names = ['kim', 'lee', 'kim', 'jin']
+names.push('park')
+console.log(names) // [ 'kim', 'lee', 'kim', 'jin', 'park' ]
+console.log(names[0]) // kim
+console.log(names.length) // 5
+
+// 객체
+
+
